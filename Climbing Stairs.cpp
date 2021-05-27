73351d8f8dfb0d4f0ed2325eabe201338b062cb4
Satyajit-Gorai <47174089+Satyajit-Gorai@users.noreply.github.com>
2021-05-27 22:36:44 +0530

    Add files via upload
    leetcode Climbing Stairs problem.

---
+++ Climbing Stairs.cpp
@@ -0,0 +1,58 @@
+/* 
+Problem Statement:-
+
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ 
+
+Constraints:
+
+1 <= n <= 45
+
+it is more of like fibonacci series where you have to print n'th fibonacci number.
+
+happy coding!!!
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int climbStairs(int n) {
+        if (n<=2) {
+            return n;
+        }
+        int x=1,y=2,temp=0;
+        for(int i=3; i<n;i++) {
+            temp = y;
+            y = x+y;
+            x=temp;
+        }
+        return (x+y);
+    }
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << climbStairs(n)<<endl;
+    return 0;
+}
